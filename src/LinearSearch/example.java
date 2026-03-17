e656a4b5d5a5d778a54ae0272e09e2b37499ac9b
mouni1727 <mounibogaraju@gmail.com>
2026-03-17 19:04:41 +0530

    LinearSearch

---
+++ src/LinearSearch/example.java
@@ -0,0 +1,26 @@
+package LinearSearch;
+
+public class example {
+    public static void main(String[] args) {
+
+        int[] example = {1 ,2 ,3 , 4 , 5,};
+        int value = 2;
+        int ans = linearSearch(example,value);
+
+        System.out.println(ans);
+    }
+    static int linearSearch(int[] example,int value) {
+        if (example.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i <= example.length; i++) {
+            int elements = example[i];
+            if (elements == value) {
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+}
+
